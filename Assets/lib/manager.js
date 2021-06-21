a155ec13be7fb5ff1c96a189e12294228329a2da
Hawa <hawa.francis@gmail.com>
2021-06-21 18:50:37 -0400

    assets

---
+++ Assets/lib/manager.js
@@ -0,0 +1,23 @@
+class Manager {
+    constructor(name, id, email, officeNumber) {
+    
+        this.name=name;
+        this.id=id;
+        this.email=email;
+        this.officeNumber=officeNumber;
+
+    };
+    getName(){
+        return this.name;
+    };
+    getID(){
+        return this.id;
+    };
+    getEmail(){
+        return this.email;
+    };
+    getOfficeNumber(){
+        return this.officeNumber;
+    };
+}
+module.exports = Manager;
