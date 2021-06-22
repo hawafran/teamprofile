54aab9064130af726a9f4024c8879449491d2407
Hawa <hawa.francis@gmail.com>
2021-06-22 04:33:32 -0400

    employee test pass

--- Assets/dist/appliction.js
+++ Assets/dist/appliction.js
@@ -1,6 +1,5 @@
 const inquirer = require("inquirer");
 const fs = require ("fs");
-
 const Engineer = require("../lib/Engineer");
 const Intern = require("../lib/intern");
 const Engineer = require("../lib/engineer");
--- Assets/lib/employee.js
+++ Assets/lib/employee.js
@@ -1,19 +1,30 @@
 class Employee {
-    constructor(name, id, email) {
-    
-        this.name=name;
-        this.id=id;
-        this.email=email;
+    constructor (name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email 
+    }
 
-    };
-    getName(){
+ 
+    getName () {
         return this.name;
-    };
-    getID(){
+    }
+
+    
+    getId () {
         return this.id;
-    };
-    getEmail(){
+    }   
+
+    
+    getEmail () {
         return this.email;
-    };
-}
-module.exports = Employee;
+    }
+
+    
+    getRole () {
+        return 'Employee'; 
+    }
+};
+
+
+module.exports = Employee; 
